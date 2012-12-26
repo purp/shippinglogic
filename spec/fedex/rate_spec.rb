237ccebe0a88bfb0a5843787f4b24db81facd14e
Jim Meyer <jim@geekdaily.org>
2012-12-26 14:23:20 -0800

    Compare as float, not big decimal

--- spec/fedex/rate_spec.rb
+++ spec/fedex/rate_spec.rb
@@ -4,41 +4,41 @@ describe "FedEx Rate" do
   before(:each) do
     setup_fedex_responses
   end
-  
+
   it "should rate the shipment" do
     use_fedex_response(:rate_defaults)
-    
+
     fedex = new_fedex
     rates = fedex.rate
-    
+
     rates.attributes = fedex_shipper
     rates.attributes = fedex_recipient
     rates.attributes = fedex_package
     rates.size.should == 6
-    
+
     rate = rates.first
     rate.name.should == "First Overnight"
     rate.type.should == "FIRST_OVERNIGHT"
     rate.saturday.should == false
     rate.delivered_by.should == Time.parse("Fri Aug 07 08:00:00 2009")
     rate.speed.should == 86400 # 1.day
-    rate.rate.should == 70.01
+    rate.rate.to_f.should == 70.01
     rate.currency.should == "USD"
   end
-  
+
   it "should not require package dimensions when not using custom packaging" do
     use_fedex_response(:rate_non_custom_packaging)
-    
+
     fedex = new_fedex
     rates = fedex.rate
-    
+
     rates.attributes = fedex_shipper
     rates.attributes = fedex_recipient
     rates.packaging_type = "FEDEX_ENVELOPE"
     rates.package_weight = 0.1
-    
+
     rates.size.should == 5
-    
+
     rate = rates.first
     rate.name.should == "First Overnight"
     rate.type.should == "FIRST_OVERNIGHT"
@@ -48,26 +48,26 @@ describe "FedEx Rate" do
     rate.rate.should == 50.43
     rate.currency.should == "USD"
   end
-  
+
   it "should handle responses with no services" do
     use_fedex_response(:rate_no_services)
-    
+
     fedex = new_fedex
     rates = fedex.rate
-    
+
     rates.attributes = fedex_shipper
     rates.attributes = fedex_recipient
     rates.packaging_type = "FEDEX_ENVELOPE"
     rates.package_weight = 2
     rates.size.should == 0
   end
-  
+
   it "should remove any services that don't meet the deadline" do
     use_fedex_response(:rate_defaults)
-    
+
     fedex = new_fedex
     rates = fedex.rate
-    
+
     rates.attributes = fedex_shipper
     rates.attributes = fedex_recipient
     rates.attributes = fedex_package
@@ -75,13 +75,13 @@ describe "FedEx Rate" do
     rates.size.should == 1
     rates.first.delivered_by.should == Time.parse("Fri Aug 07 08:00:00 2009")
   end
-  
+
   it "should rate the shipment with an insured value" do
     use_fedex_response(:rate_insurance)
-    
+
     fedex = new_fedex
     rates = fedex.rate
-    
+
     rates.attributes = fedex_shipper
     rates.attributes = fedex_recipient
     rates.attributes = fedex_package
